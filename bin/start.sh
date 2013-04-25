9a65be4570b1d7d9661ea377b8c238fa25c1c242
llohellohe <yangqichris@gmail.com>
2013-04-25 19:25:32 +0800

    add

---
+++ bin/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z $WINSTONE_HOME ];then
+WINSTONE_HOME=/Users/yangqi/Dropbox/open-code/winstone
+fi
+
+echo "Winstone Home is $WINSTONE_HOME"
+
+#JAVA_DEBUG_OPT=" -server -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=1050,server=y,suspend=y "
+java $JAVA_DEBUG_OPT -jar $WINSTONE_HOME/winstone-0.9.10.jar --webappsDir=webapp $*
